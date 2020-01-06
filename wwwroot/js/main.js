c28d1a624ab3c6d57a30c34368e5ce82878ae68f
Богомолов Владимир <vvdasitvv@mail.ru>
2020-01-06 16:43:11 +0200

    Modal windows is implemented

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -1,1 +1,63 @@
-﻿console.log("Hello world");
+﻿let modalWindow =
+    [
+        document.querySelector(".modalWrap"),
+        document.querySelector(".modalWindow")
+    ];
+
+function CloseModalWindow()
+{
+    modalWindow.forEach((el) =>
+    {
+        el.classList.add("hide");
+    });
+
+    addEventListener("keydown", null);
+}
+
+function ShowModalWindow()
+{
+    modalWindow.forEach((el) =>
+    {
+        el.classList.remove("hide");
+    });
+
+    addEventListener("keydown", (e) =>
+    {
+        if (e.key == "Escape")
+        {
+            CloseModalWindow();
+        }
+    });
+}
+
+
+
+document.querySelector("#close-window").onclick = () =>
+{
+    CloseModalWindow();
+};
+
+document.querySelector('#show-window').onclick = () =>
+{
+    ShowModalWindow();
+};
+
+document.querySelector(".modalWrap").onclick = (e) =>
+{
+    if (e.target.className == "modalWrap")
+    {
+        CloseModalWindow();
+    }
+    else
+    {
+        return false;
+    }
+};
+
+addEventListener("keydown", (e) =>
+{
+    if (e.key == "Escape")
+    {
+        CloseModalWindow();
+    }
+})
